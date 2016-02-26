d7922bbc63996dafa0221c51a19786d733a6c8ef
kaylaboch <boch.kayla57@gmail.com>
2016-02-26 16:53:22 -0500

    meta

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ var looperLetter = prompt('Give me a letter!');
 var i;
 
 for (i = 0; i < 10; i++) {
-  document.write(looperLetter)
-  document.write('<br>')
+  document.write(looperLetter);
+  document.write('<br>');
 }
